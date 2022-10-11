b53c1ad917cb90484f26bcc3f145d0493a3074a5
walter <jacojacob710@gmail.com>
2022-10-11 00:47:32 -0300

    Initial commit

---
+++ src/EjerciciosTema3/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        sumar(14, 9, 2);
+
+        Coche miCoche = new Coche();
+        miCoche.agregarPuerta();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static void sumar(int x, int y, int z) {
+        int resultado = x + y + z;
+        System.out.println(resultado);
+    }
+}
+
+class Coche {
+    public int puertas = 3;
+
+    public void agregarPuerta() {
+        this.puertas++;
+    }
+}
